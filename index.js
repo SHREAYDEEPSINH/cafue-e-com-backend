730321e697ca20a56f6020839c5992b649976b66
SHREAYDEEPSINH <shreaydeepsinhvaghela@gmail.com>
2025-02-13 22:38:42 +0530

    backend

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ const path = require("path")
 
 dotenv.config()
 const app = express()
-app.use(cors({
-    origin: 'https://cafeu-e-com-frontend.vercel.app',
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization'
-}))
+app.use(cors())
 app.use(express.json())
 
 app.use(express.urlencoded())
