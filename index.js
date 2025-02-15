dcc06e8768e1a3d54b90a932b33b4af50774d644
SHREAYDEEPSINH <shreaydeepsinhvaghela@gmail.com>
2025-02-15 15:48:10 +0530

    cafeu-backend

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const path = require("path")
 
 dotenv.config()
 const app = express()
+app.use(cors());
+
 app.use(express.json())
 
 app.use(express.urlencoded())
-app.use(cors({ origin: "*" }));
+
 
 app.get("/" , async (req,res)=>{
    await res.send("Hello server")
