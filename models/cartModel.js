1c38afb46cf02d23020aa2b85bb76d118cce17a1
SHREAYDEEPSINH <shreaydeepsinhvaghela@gmail.com>
2025-02-24 15:46:16 +0530

    cafeu-backend

--- models/cartModel.js
+++ models/cartModel.js
@@ -12,6 +12,10 @@ const cartSchema = mongoose.Schema({
         required: true,
         ref: "Product"
     },
+    productImage:{
+        type: String,
+        required: true
+    },
     productName: {
         type: String,
         required: true
--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -20,12 +20,13 @@ cartRouter.get("/", auth, async (req, res) => {
 // Add item to cart
 cartRouter.post("/addcart", auth, async (req, res) => {
     try {
-        const { productId, productName, productPrice, quantity } = req.body;
-        const userId = req.body.cartId; 
+        const { productId, productImage, productName, productPrice, quantity } = req.body;
+        const userId = req.body.cartId;
 
         const newCartItem = new CartModel({
             cartId: userId,
             productId,
+            productImage,
             productName,
             productPrice,
             quantity: quantity || 1
@@ -41,7 +42,7 @@ cartRouter.post("/addcart", auth, async (req, res) => {
 
 
 
-cartRouter.delete("/deletecart/:id" , async (req,res)=>{
+cartRouter.delete("/deletecart/:id", async (req, res) => {
     try {
         await CartModel.findByIdAndDelete(req.params.id);
         res.status(200).json({ message: "cart deletes successfully" });
@@ -50,7 +51,7 @@ cartRouter.delete("/deletecart/:id" , async (req,res)=>{
     }
 })
 
-cartRouter.put("/editcart/:id" , async (req,res)=>{
+cartRouter.put("/editcart/:id", async (req, res) => {
     try {
         await CartModel.findByIdAndUpdate(req.params.id, req.body);
         res.status(200).json({ message: "cart edited successfully" });
